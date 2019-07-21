7b5a345cdea6b9422155a5bd01f13cb187d47bd1
Daniel Lee <leedc@me.com>
2019-07-21 19:19:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 
 def assign_rooms(attendees)
   room_assignments = []
-  attendees.each_with_index { |name, index| room_assignments.push("Hello, #{name}! You'll be assigned to room #{index}!") }
+  attendees.each_with_index { |name, index| room_assignments.push("Hello, #{name}! You'll be assigned to room #{index + 1}!") }
   return room_assignments
 end
 
