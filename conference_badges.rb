f73cf80aec6c0364fca487f4fbd0c89727cb64fe
Daniel Lee <leedc@me.com>
2019-07-21 17:53:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each { |badges| badge_maker(name = badges) }
+end
+
+def assign_rooms()
+
+end
+
+def printer()
+
+end
