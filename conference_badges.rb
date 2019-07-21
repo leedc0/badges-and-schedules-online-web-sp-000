7ec46e252bfd7154aff9a8d1f6aeddc4b82bc980
Daniel Lee <leedc@me.com>
2019-07-21 18:53:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,10 +9,13 @@ def batch_badge_creator(attendees)
   return badges
 end
 
-def assign_rooms()
-
+def assign_rooms(attendees)
+  assign[]
+  attendees.each_with_index { |name, index| assign.push("Hello, #{name}! You'll be assigned to room #{index}!") }
+  return assign
 end
 
-def printer()
-
+def printer(room_assignments)
+  batch_badge_creator(attendees).each { |badge| puts badge }
+  assign_rooms(attendees).each { |badge| puts badge }
 end
